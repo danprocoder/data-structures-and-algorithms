fe99cb3bcb64341be096aa6f04eef1b5418bd400
Daniel Austin <danielaustin718@gmail.com>
2019-08-28 17:47:13 +0100

    Get number at position in fibonacci series

--- recursive/fibonacci.cpp
+++ recursive/fibonacci.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
@@ -7,7 +8,7 @@ using namespace std;
  * A series of number in which each digit is the sum of the preceding 2 digits. The first 2 numbers are 0 and 1.
  */
 
-void fibTill(int stop) {
+void fib_till(int stop) {
   static int a = 0, b = 1;
 
   if (a == 0 && b == 1) {
@@ -21,7 +22,18 @@ void fibTill(int stop) {
   b = c;
 
   if (b < stop)
-    fibTill(stop);
+    fib_till(stop);
+}
+
+int fib_at(int point) {
+  if (point < 1) {
+    cerr << "Point should be at least 1" << endl << endl;
+    exit(EXIT_FAILURE);
+  } else if (point <= 2) {
+    return point - 1;
+  } else {
+    return fib_at(point - 1) + fib_at(point - 2);
+  }
 }
 
 int main(int argc, char* argv[]) {
@@ -35,12 +47,15 @@ int main(int argc, char* argv[]) {
   int stop = stoi(argv[2]);
 
   if (strcmp(op, "till") == 0) {
-    fibTill(stop);
+    cout << "Fibonacci series from 0 to " << stop << endl;
+    fib_till(stop);
   } else if (strcmp(op, "at") == 0) {
-
+    int fib_num = fib_at(stop);
+    cout << "Number at position " << stop << " in the fibonacci series: " << endl << fib_num << endl;
   } else {
     cout << "Unrecognized operation \"" << op << "\"" << endl;
   }
+  cout << endl;
 
   return 0;
 }
